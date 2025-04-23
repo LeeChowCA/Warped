2e57c4fd6bfabcb5d1ad2167100c76eeeacbb6eb
Lee <zhoujianpingls@outlook.com>
2025-04-22 22:09:14 -0700

    can adjust the volume of sound effect and music

--- Assets/Scripts/SettingsPopup.cs
+++ Assets/Scripts/SettingsPopup.cs
@@ -1,12 +1,25 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsPopup : BasePopup
 {
     [SerializeField] private OptionsPopup optionsPopup;
+    [SerializeField] private TextMeshProUGUI volumeLabel;
+    [SerializeField] private Slider VolumeSlider;
+
+    private SoundManager soundManager;
+
+    private void Awake()
+    {
+        soundManager = Object.FindFirstObjectByType<SoundManager>();
+        VolumeSlider.value = soundManager.MusicVolume;
+    }
 
     public void OnOKButton()
     {
+
+
         base.Close();
         //Messenger.Broadcast(GameEvent.POPUP_OPENED);
         optionsPopup.Open();
@@ -25,5 +38,18 @@ public class SettingsPopup : BasePopup
         Messenger.Broadcast(GameEvent.POPUP_OPENED);
 
         gameObject.SetActive(true);
+    }
+
+
+    public void OnMusicVolumeChanged(float value)
+    {
+        Debug.Log("Music vol changed to:" + value);
+        SoundManager.Instance.MusicVolume = value;
+    }
+
+    public void OnPlaySfx(float value)
+    {
+        Debug.Log("OnPlaySfx()");
+        SoundManager.Instance.SfxVolume = value;
     }
 }
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SoundManager:MonoBehaviour
 {
+    static public SoundManager Instance;
+
     [SerializeField] private AudioSource playerSfxSource;
     [SerializeField] private AudioSource musicSource;
     [SerializeField] private AudioSource eggTurretSfxSource;
@@ -9,9 +12,19 @@ public class SoundManager:MonoBehaviour
     
     [SerializeField] private AudioClip musicTrack;
 
+    private float sfxVolume = 1.0f;     // for tracking sfx volume
+    private float musicVolume = 1.0f;   // for tracking music volume
+
+    [SerializeField] private AudioMixer mixer;
+
     private void Awake()
     {
         PlayMusic(musicTrack);
+
+        if (Instance == null)                    // if Awake() has never been called before
+        {
+            Instance = this;                    // remember this as our (one & only) SM
+        }
     }
 
     public void PlayPlayerSfx(AudioClip clip, float volume = 1.0f)
@@ -36,5 +49,32 @@ public class SoundManager:MonoBehaviour
     public void StopMusic()
     {
         //musicSource.Stop();
+    }
+
+    public float SfxVolume
+    {
+        get { return sfxVolume; }
+        set
+        {
+            sfxVolume = Mathf.Clamp(value, 0.0f, 1.0f);
+            mixer.SetFloat("SfxVolume", LinearToLog(sfxVolume));
+        }
+    }
+
+    //// a property to get/set music volume
+    public float MusicVolume
+    {
+        get { return musicVolume; }
+        set
+        {
+            musicVolume = Mathf.Clamp(value, 0.0f, 1.0f);
+            mixer.SetFloat("MusicVolume", LinearToLog(musicVolume));
+            Debug.Log("Music volume set to: " + musicVolume);
+        }
+    }
+
+    private float LinearToLog(float value)
+    {
+        return Mathf.Log10(value) * 20;
     }
 }
